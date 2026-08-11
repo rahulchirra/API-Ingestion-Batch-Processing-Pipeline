22561aebc6e80084a7fd7733d88cdb6e4fce6046
rahul chirra <94059294+rahulchirra@users.noreply.github.com>
2026-08-11 10:11:46 +0000

    Completed End-to-End ETL Pipeline with Airflow and Postgres

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# API-Ingestion-Batch-Processing-Pipeline
+# End-to-End Automated Crypto ETL Pipeline
+
+## Overview
+This project is a fully automated Data Engineering pipeline that extracts real-time cryptocurrency data from the Binance API, transforms it, and loads it into a PostgreSQL data warehouse. The entire workflow is containerized using Docker and orchestrated to run daily using Apache Airflow.
+
+## Tech Stack
+* **Data Source:** Binance Public API
+* **Language:** Python (requests, pandas)
+* **Destination:** PostgreSQL
+* **Infrastructure:** Docker & Docker Compose
+* **Orchestration:** Apache Airflow
+
+
+## How to Run Locally
+1. Clone the repository.
+2. Start the database:
+   `docker-compose up -d`
+3. Install dependencies:
+   `pip install -r requirements.txt`
+4. Start Airflow:
+   `airflow standalone`
+5. Access the Airflow UI at `localhost:8080` and trigger the `crypto_daily_etl_pipeline` DAG.
---
+++ elt.py
@@ -0,0 +1,66 @@
+import requests
+import psycopg2
+from datetime import datetime
+
+DB_HOST = "localhost"
+DB_NAME = "weather_db"
+DB_USER = "admin"
+DB_PASS = "adminpassword"
+
+def create_table_if_not_exists(cursor):
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS crypto_prices (
+        id SERIAL PRIMARY KEY,
+        coin_name VARCHAR(50),
+        symbol VARCHAR(50),
+        price_usd DECIMAL,
+        inserted_at TIMESTAMP
+    );
+    """
+    cursor.execute(create_table_query)
+
+def extract_data():
+    # CoinCap badulu Binance API vadutunnam (No DNS blocks)
+    print("Extracting data from Binance API...")
+    url = "https://api.binance.com/api/v3/ticker/price"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        return response.json()[:10] # Top 10 coins
+    else:
+        print("API failed!")
+        return []
+
+def load_data(data):
+    try:
+        conn = psycopg2.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASS)
+        cursor = conn.cursor()
+        create_table_if_not_exists(cursor)
+        
+        insert_query = """
+        INSERT INTO crypto_prices (coin_name, symbol, price_usd, inserted_at)
+        VALUES (%s, %s, %s, %s)
+        """
+        
+        print("Loading data into PostgreSQL...")
+        for coin in data:
+            # Binance nunchi 'price' and 'symbol' vastundi
+            price = round(float(coin['price']), 2) 
+            current_time = datetime.now()
+            
+            # Name and Symbol rendu chotla coin['symbol'] isthunnam (e.g., BTCUSDT)
+            cursor.execute(insert_query, (coin['symbol'], coin['symbol'], price, current_time))
+            
+        conn.commit()
+        print("Successfully loaded 10 records into the database!")
+        
+    except Exception as e:
+        print(f"Database error: {e}")
+    finally:
+        if cursor: cursor.close()
+        if conn: conn.close()
+
+if __name__ == "__main__":
+    raw_data = extract_data()
+    if raw_data:
+        load_data(raw_data)
